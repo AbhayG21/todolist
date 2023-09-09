3e5e918a43c4f08dd8e46795f8832d8aa3b3c547
AbhayG21 <abhay19021@gmail.com>
2023-09-09 21:44:40 +0530

    2 way binding

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 
 const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
     return (
         <>
             <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
@@ -9,13 +12,21 @@ const page = () => {
             <form>
                 <input
                     type="text"
-                    className="text-2xl border-zinc-800 border-2 m-8 px-4 py-2"
-                    placeholder="Enter your task here"
+                    className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2"
+                    placeholder="Enter your title here"
+                    value={title}
+                    onChange={(e)=>{
+                      settitle(e.target.value)
+                    }}
                 />
                 <input
                     type="text"
-                    className="text-2xl border-zinc-800 border-2 m-8 px-4 py-2"
-                    placeholder="Enter your task here"
+                    className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2"
+                    placeholder="Enter your description here"
+                    value={desc}
+                    onChange={(e)=>{
+                      setdesc(e.target.value)
+                    }}
                 />
                 <button className="bg-black text-white px-4 py-3 m-5 text-2xl font-bold rounded">Add Task</button>
             </form>
